e18442150f8826e7351c1d3ae69d4c762d947a9d
Sina Serati <WinterSina@Sinas-MacBook-Pro.local>
2016-08-14 10:44:01 -0500

    sync

---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        SpawnObject = SpawnObjects[Random.Range(0, SpawnObjects.Length)];
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        if (GameStateManager.GameState == GameState.Playing)
+        {
+            //random y position
+            float y = Random.Range( 1f, 2f);
+            GameObject go = Instantiate(SpawnObject, this.transform.position + new Vector3(0, y, 0), Quaternion.identity) as GameObject;
+        }
+        Invoke("Spawn", Random.Range(timeMin, timeMax));
+    }
+
+    private GameObject SpawnObject;
+    public GameObject[] SpawnObjects;
+
+    public float timeMin = 2f;
+    public float timeMax = 4f;
+}
